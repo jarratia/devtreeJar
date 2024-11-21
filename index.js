d9d7b49df18383cfdcba1163cf79f1929c8ed7a0
jarratia <jarratia34@gmail.com>
2024-11-21 07:47:59 -0400

    Creando el servidor de express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hola mundo");
+const express = require("express");
+
+const app = express();
+
+//Definición de routing
+app.get("/", (req, res) => {
+  res.send("Hola mundo con express");
+});
+
+app.listen(4000, () => console.log("Servidor funcionando"));
